e7d37a5052dbc8a7204c8b8d5c68a2532228c27c
akash0751 <akashsofficial05@gmail.com>
2025-02-26 22:21:50 +0530

    Add files via upload

---
+++ day 7/task 8/model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const StudentSchema = mongoose.Schema({
+    rollNo: {
+        type: String,
+        required: [true, "Roll Number is required"],
+        unique: true,
+        match: [/^\d{4,10}$/, "Roll Number must be 4-10 digits"],
+    },
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        minlength: [3, "Name must be at least 3 characters"],
+        maxlength: [50, "Name must be at most 50 characters"],
+    }}
+    ,{
+        Timestamps :true
+    }
+);
+
+const Student = mongoose.model('Student',StudentSchema)
+module.exports = Student;
